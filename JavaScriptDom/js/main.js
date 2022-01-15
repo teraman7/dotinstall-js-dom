1712cfa042ded6c6b6bb48edca3fe0e015c38577
teraman7 <puman0815@gmail.com>
2022-01-15 16:29:55 +0900

    lesson 2021-12-19

--- JavaScriptDom/js/main.js
+++ JavaScriptDom/js/main.js
@@ -1,17 +1,9 @@
 'use strict';
 
 {
-  document.querySelector('button').addEventListener('click', () => {
-    const targetNode = document.getElementById('target');
-    
-    // targetNode.className = 'my-color my-border';
-    // targetNode.classList.add('my-color');
-    // if (targetNode.classList.contains('my-color') === true ) {
-    //   targetNode.classList.remove('my-color');
-    // } else {
-    //   targetNode.classList.add('my-color');
-    // }
-
-    targetNode.classList.toggle('my-color');
+  document.querySelector('ul').addEventListener('click', e => {
+    if (e.target.nodeName === 'LI') { //Element.tagNameはタグ名を大文字で返す仕様。そのため、'LI'と比較する。
+      e.target.classList.toggle('done');
+    }
   });
 }
